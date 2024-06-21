d81c4adfeb23a113a79d3d62e7db415d30da5ae3
Anton Novojilov <andy@essentialkaos.com>
2024-06-22 01:44:19 +0300

    The very first version

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# siocrypt
-Tool for encrypting/decrypting arbitrary data streams
+<p align="center"><a href="#readme"><img src=".github/images/card.svg"/></a></p>
+
+<p align="center">
+  <a href="https://kaos.sh/r/siocrypt"><img src="https://kaos.sh/r/siocrypt.svg" alt="GoReportCard" /></a>
+  <a href="https://kaos.sh/l/siocrypt"><img src="https://kaos.sh/l/21be11a0cf23f4dcea42.svg" alt="Code Climate Maintainability" /></a>
+  <a href="https://kaos.sh/b/siocrypt"><img src="https://kaos.sh/b/07a41351-9d6d-45b1-9a02-344ef3b50466.svg" alt="Codebeat badge" /></a>
+  <a href="https://kaos.sh/w/siocrypt/ci"><img src="https://kaos.sh/w/siocrypt/ci.svg" alt="GitHub Actions CI Status" /></a>
+  <a href="https://kaos.sh/w/siocrypt/codeql"><img src="https://kaos.sh/w/siocrypt/codeql.svg" alt="GitHub Actions CodeQL Status" /></a>
+  <a href="#license"><img src=".github/images/license.svg"/></a>
+</p>
+
+<p align="center"><a href="#installation">Installation</a> • <a href="#command-line-completion">Command-line completion</a> • <a href="#man-documentation">Man documentation</a> • <a href="#usage">Usage</a> • <a href="#ci-status">CI Status</a> • <a href="#contributing">Contributing</a> • <a href="#license">License</a></p>
+
+<br/>
+
+`siocrypt` is a tool for encrypting/decrypting arbitrary data streams.
+
+### Installation
+
+#### From source
+
+To build the `siocrypt` from scratch, make sure you have a working Go 1.20+ workspace (_[instructions](https://go.dev/doc/install)_), then:
+
+```
+go install github.com/essentialkaos/siocrypt@latest
+```
+
+#### Container Image
+
+The latest version of `siocrypt` also available as container image on [GitHub Container Registry](https://kaos.sh/p/siocrypt) and [Docker Hub](https://kaos.sh/d/siocrypt):
+
+```bash
+podman run --rm -it ghcr.io/essentialkaos/siocrypt:latest
+# or
+docker run --rm -it ghcr.io/essentialkaos/siocrypt:latest
+```
+
+#### Prebuilt binaries
+
+You can download prebuilt binaries for Linux and macOS from [EK Apps Repository](https://apps.kaos.st/siocrypt/latest):
+
+```bash
+bash <(curl -fsSL https://apps.kaos.st/get) siocrypt
+```
+
+### Command-line completion
+
+You can generate completion for `bash`, `zsh` or `fish` shell.
+
+Bash:
+```bash
+sudo siocrypt --completion=bash 1> /etc/bash_completion.d/siocrypt
+```
+
+ZSH:
+```bash
+sudo siocrypt --completion=zsh 1> /usr/share/zsh/site-functions/siocrypt
+```
+
+Fish:
+```bash
+sudo siocrypt --completion=fish 1> /usr/share/fish/vendor_completions.d/siocrypt.fish
+```
+
+### Man documentation
+
+You can generate man page using next command:
+
+```bash
+siocrypt --generate-man | sudo gzip > /usr/share/man/man1/siocrypt.1.gz
+```
+
+### Usage
+
+<p align="center"><img src=".github/images/usage.svg"/></p>
+
+### CI Status
+
+| Branch | Status |
+|--------|----------|
+| `master` | [![CI](https://kaos.sh/w/siocrypt/ci.svg?branch=master)](https://kaos.sh/w/siocrypt/ci?query=branch:master) |
+| `develop` | [![CI](https://kaos.sh/w/siocrypt/ci.svg?branch=develop)](https://kaos.sh/w/siocrypt/ci?query=branch:develop) |
+
+### Contributing
+
+Before contributing to this project please read our [Contributing Guidelines](https://github.com/essentialkaos/contributing-guidelines#contributing-guidelines).
+
+### License
+
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+<p align="center"><a href="https://essentialkaos.com"><img src="https://gh.kaos.st/ekgh.svg"/></a></p>
---
+++ cli/cli.go
@@ -0,0 +1,468 @@
+package cli
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2024 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/essentialkaos/ek/v12/errutil"
+	"github.com/essentialkaos/ek/v12/fmtc"
+	"github.com/essentialkaos/ek/v12/fsutil"
+	"github.com/essentialkaos/ek/v12/options"
+	"github.com/essentialkaos/ek/v12/secstr"
+	"github.com/essentialkaos/ek/v12/support"
+	"github.com/essentialkaos/ek/v12/support/deps"
+	"github.com/essentialkaos/ek/v12/system/procname"
+	"github.com/essentialkaos/ek/v12/terminal"
+	"github.com/essentialkaos/ek/v12/terminal/tty"
+	"github.com/essentialkaos/ek/v12/usage"
+	"github.com/essentialkaos/ek/v12/usage/completion/bash"
+	"github.com/essentialkaos/ek/v12/usage/completion/fish"
+	"github.com/essentialkaos/ek/v12/usage/completion/zsh"
+	"github.com/essentialkaos/ek/v12/usage/man"
+	"github.com/essentialkaos/ek/v12/usage/update"
+
+	"golang.org/x/crypto/scrypt"
+
+	"github.com/essentialkaos/sio"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Basic utility info
+const (
+	APP  = "siocrypt"
+	VER  = "0.0.1"
+	DESC = "Tool for encrypting/decrypting arbitrary data streams"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Options
+const (
+	OPT_DECRYPT  = "D:decrypt"
+	OPT_CIPHER   = "c:cipher"
+	OPT_PASSWORD = "p:password"
+	OPT_NO_COLOR = "nc:no-color"
+	OPT_HELP     = "h:help"
+	OPT_VER      = "v:version"
+
+	OPT_VERB_VER     = "vv:verbose-version"
+	OPT_COMPLETION   = "completion"
+	OPT_GENERATE_MAN = "generate-man"
+)
+
+const (
+	CIPHER_AES256   = "AES256"   // AES-256 GCM
+	CIPHER_C20P1305 = "C20P1305" // ChaCha20 Poly1305
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// optMap contains information about all supported options
+var optMap = options.Map{
+	OPT_DECRYPT:  {Type: options.BOOL},
+	OPT_CIPHER:   {},
+	OPT_PASSWORD: {},
+	OPT_NO_COLOR: {Type: options.BOOL},
+	OPT_HELP:     {Type: options.BOOL},
+	OPT_VER:      {Type: options.MIXED},
+
+	OPT_VERB_VER:     {Type: options.BOOL},
+	OPT_COMPLETION:   {},
+	OPT_GENERATE_MAN: {Type: options.BOOL},
+}
+
+// color tags for app name and version
+var colorTagApp, colorTagVer string
+
+// password is secure string with password
+var password *secstr.String
+
+// removeOutputOnError is flag to remove output file on error
+var removeOutputOnError bool
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Run is main utility function
+func Run(gitRev string, gomod []byte) {
+	preConfigureUI()
+
+	args, errs := options.Parse(optMap)
+
+	if !errs.IsEmpty() {
+		terminal.Error("Options parsing errors:")
+		terminal.Error(errs.String())
+		os.Exit(1)
+	}
+
+	configureUI()
+
+	switch {
+	case options.Has(OPT_COMPLETION):
+		os.Exit(printCompletion())
+	case options.Has(OPT_GENERATE_MAN):
+		printMan()
+		os.Exit(0)
+	case options.GetB(OPT_VER):
+		genAbout(gitRev).Print(options.GetS(OPT_VER))
+		os.Exit(0)
+	case options.GetB(OPT_VERB_VER):
+		support.Collect(APP, VER).
+			WithRevision(gitRev).
+			WithDeps(deps.Extract(gomod)).
+			Print()
+		os.Exit(0)
+	case options.GetB(OPT_HELP) || (!hasStdinData() && len(args) == 0):
+		genUsage().Print()
+		os.Exit(0)
+	}
+
+	err := errutil.Chain(
+		processPassword,
+		validateOptions,
+	)
+
+	if err != nil {
+		terminal.Error(err)
+		os.Exit(1)
+	}
+
+	err = process(args)
+
+	if err != nil {
+		terminal.Error(err)
+		os.Exit(1)
+	}
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// preConfigureUI preconfigures UI based on information about user terminal
+func preConfigureUI() {
+	if !tty.IsTTY() {
+		fmtc.DisableColors = true
+	}
+
+	switch {
+	case fmtc.IsTrueColorSupported():
+		colorTagApp, colorTagVer = "{*}{#6100DC}", "{#6100DC}"
+	case fmtc.Is256ColorsSupported():
+		colorTagApp, colorTagVer = "{*}{#56}", "{#56}"
+	default:
+		colorTagApp, colorTagVer = "{*}{c}", "{c}"
+	}
+}
+
+// configureUI configures user interface
+func configureUI() {
+	if options.GetB(OPT_NO_COLOR) {
+		fmtc.DisableColors = true
+	}
+}
+
+// processPassword tries to securily read password from options or env vars
+func processPassword() error {
+	var err error
+
+	pwd := options.GetS(OPT_PASSWORD)
+
+	if pwd != "" {
+		err = procname.Replace(pwd, strings.Repeat("*", len(pwd)))
+
+		if err != nil {
+			return fmt.Errorf("Can't hide provided password: %v", err)
+		}
+	} else if os.Getenv("SIOCRYPT_PASSWORD") != "" {
+		pwd = os.Getenv("SIOCRYPT_PASSWORD")
+		os.Setenv("SIOCRYPT_PASSWORD", "")
+	}
+
+	if pwd == "" {
+		return fmt.Errorf("Password is not set")
+	}
+
+	password, err = secstr.NewSecureString(pwd)
+
+	if err != nil {
+		return fmt.Errorf("Can't create secure string for password: %v", err)
+	}
+
+	return nil
+}
+
+// validateOptions validates options
+func validateOptions() error {
+	if options.Has(OPT_CIPHER) {
+		switch strings.ToUpper(options.GetS(OPT_CIPHER)) {
+		case CIPHER_AES256, CIPHER_C20P1305:
+			// ok
+		default:
+			return fmt.Errorf("Unsupported cipher suite %q", options.GetS(OPT_CIPHER))
+		}
+	}
+
+	return nil
+}
+
+// process starts arguments processing
+func process(args options.Arguments) error {
+	input, output, err := getIO(args)
+
+	if err != nil {
+		return err
+	}
+
+	key, err := deriveKey(input, output)
+
+	if err != nil {
+		return err
+	}
+
+	cfg := sio.Config{Key: key, CipherSuites: getCipherSuite()}
+
+	if options.GetB(OPT_DECRYPT) {
+		_, err = sio.Decrypt(output, input, cfg)
+
+		if err != nil {
+			cleanupOutput(output.Name())
+			return fmt.Errorf("Error while decrypting data: %v", err)
+		}
+	} else {
+		_, err = sio.Encrypt(output, input, cfg)
+
+		if err != nil {
+			cleanupOutput(output.Name())
+			return fmt.Errorf("Error while encrypting data: %v", err)
+		}
+	}
+
+	return nil
+}
+
+// getIO returns input and output targets
+func getIO(args options.Arguments) (*os.File, *os.File, error) {
+	var err error
+	var input, output *os.File
+	var inputFile, outputFile string
+
+	switch len(args) {
+	case 0:
+		input = os.Stdin
+		output = os.Stdout
+	case 1:
+		inputFile = args.Get(0).Clean().String()
+		output = os.Stdout
+	default:
+		if args.Get(0).Is("--") {
+			input = os.Stdin
+		} else {
+			inputFile = args.Get(0).Clean().String()
+		}
+
+		outputFile = args.Get(1).String()
+	}
+
+	if inputFile != "" {
+		err = fsutil.ValidatePerms("FRS", inputFile)
+
+		if err != nil {
+			return nil, nil, fmt.Errorf("Can't use given input: %v", err)
+		}
+
+		input, err = os.OpenFile(inputFile, os.O_RDONLY, 0)
+
+		if err != nil {
+			return nil, nil, fmt.Errorf("Can't use given input: %v", err)
+		}
+	}
+
+	if outputFile != "" {
+		output, err = os.OpenFile(outputFile, os.O_CREATE|os.O_WRONLY, 0600)
+
+		if err != nil {
+			return nil, nil, fmt.Errorf("Can't use given output: %v", err)
+		}
+
+		removeOutputOnError = true
+	}
+
+	return input, output, err
+}
+
+// deriveKey creates derived key from password and salt
+func deriveKey(input, output *os.File) ([]byte, error) {
+	salt := make([]byte, 32)
+
+	if options.GetB(OPT_DECRYPT) {
+		_, err := io.ReadFull(input, salt)
+
+		if err != nil {
+			return nil, fmt.Errorf("Can't read salt from encrypted data: %v", err)
+		}
+	} else {
+		_, err := io.ReadFull(rand.Reader, salt)
+
+		if err != nil {
+			return nil, fmt.Errorf("Can't generate random salt: %v", err)
+		}
+
+		_, err = output.Write(salt)
+
+		if err != nil {
+			return nil, fmt.Errorf("Can't write salt to output: %v", err)
+		}
+	}
+
+	key, err := scrypt.Key(password.Data, salt, 32768, 16, 1, 32)
+
+	if err != nil {
+		return nil, fmt.Errorf("Can't derive key from password: %v", err)
+	}
+
+	password.Destroy()
+
+	return key, nil
+}
+
+// getCipherSuite returns target cipher suite
+func getCipherSuite() []byte {
+	switch strings.ToUpper(options.GetS(OPT_CIPHER)) {
+	case CIPHER_AES256:
+		return []byte{sio.AES_256_GCM}
+	case CIPHER_C20P1305:
+		return []byte{sio.CHACHA20_POLY1305}
+	}
+
+	return []byte{}
+}
+
+// hasStdinData return true if there is some data in stdin
+func hasStdinData() bool {
+	stdin, err := os.Stdin.Stat()
+
+	if err != nil {
+		return false
+	}
+
+	if stdin.Mode()&os.ModeCharDevice != 0 {
+		return false
+	}
+
+	return true
+}
+
+// cleanupOutput removes output file on error
+func cleanupOutput(file string) {
+	if removeOutputOnError {
+		os.Remove(file)
+	}
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// printCompletion prints completion for given shell
+func printCompletion() int {
+	info := genUsage()
+
+	switch options.GetS(OPT_COMPLETION) {
+	case "bash":
+		fmt.Print(bash.Generate(info, APP))
+	case "fish":
+		fmt.Print(fish.Generate(info, APP))
+	case "zsh":
+		fmt.Print(zsh.Generate(info, optMap, APP))
+	default:
+		return 1
+	}
+
+	return 0
+}
+
+// printMan prints man page
+func printMan() {
+	fmt.Println(man.Generate(genUsage(), genAbout("")))
+}
+
+// genUsage generates usage info
+func genUsage() *usage.Info {
+	info := usage.NewInfo("", "?input", "?output")
+
+	info.AppNameColorTag = colorTagApp
+
+	info.Spoiler = `  Without an input file, tool reads data from standard input and writes to standard
+  output if no output file is specified.
+
+  Password provided via {g}--password{!}/{g}-p{!} option will be masked in process command. Also, 
+  password can't be specified using {b}SIOCRYPT_PASSWORD{!} environment variable.`
+
+	info.AddOption(OPT_DECRYPT, "Decrypt data")
+	info.AddOption(OPT_PASSWORD, "Password for encrypting/decrypting", "?password")
+	info.AddOption(OPT_CIPHER, "Cipher to use {s}(AES256/C20P1305){!}", "cipher")
+	info.AddOption(OPT_NO_COLOR, "Disable colors in output")
+	info.AddOption(OPT_HELP, "Show this help message")
+	info.AddOption(OPT_VER, "Show version")
+
+	info.AddRawExample(
+		"cat data.enc | siocrypt -p TeSt1234 | grep John",
+		"Read data from standard input and grep over decrypted output",
+	)
+
+	info.AddRawExample(
+		"cat data.enc | siocrypt -p TeSt1234 -- data.txt",
+		"Read data from standard input and save decrypted data as data.txt",
+	)
+
+	info.AddExample(
+		"-p TeSt1234 data.enc | grep John",
+		"Read data from file data.enc and grep over decrypted output",
+	)
+
+	info.AddExample(
+		"-p TeSt1234 data.enc data.txt",
+		"Read data from file data.enc and save decrypted data as data.txt",
+	)
+
+	info.AddRawExample(
+		"SIOCRYPT_PASSWORD=TeSt1234 siocrypt data.enc data.txt",
+		"Use password from environment variable while decrypting data",
+	)
+
+	return info
+}
+
+// genAbout generates info about version
+func genAbout(gitRev string) *usage.About {
+	about := &usage.About{
+		App:     APP,
+		Version: VER,
+		Desc:    DESC,
+		Year:    2009,
+		Owner:   "ESSENTIAL KAOS",
+
+		AppNameColorTag: colorTagApp,
+		VersionColorTag: colorTagVer,
+		DescSeparator:   "{s}—{!}",
+
+		License:       "Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>",
+		BugTracker:    "https://github.com/essentialkaos/siocrypt/issues",
+		UpdateChecker: usage.UpdateChecker{"essentialkaos/siocrypt", update.GitHubChecker},
+	}
+
+	if gitRev != "" {
+		about.Build = "git:" + gitRev
+	}
+
+	return about
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
